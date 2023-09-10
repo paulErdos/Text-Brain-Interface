d7c571dca1162233a041edde86c9c85db58effb2
paulErdos <vincesteffens@gmail.com>
2023-09-10 02:31:17 -0700

    Added pause / play button, moved text corpus to separate file

---
+++ Text-Brain-Interface/ContentView.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView.swift
+//  Text-Brain-Interface
+//
+//  Created by Vincent Steffens on 9/6/23.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State var speed = 0.0
+    @State var isEditing = false
+    @State private var displayIndex = 0
+    @State private var isPaused = false
+    //@Binding var text: String
+    //@Binding var entered_entry: String
+    
+    var body: some View {
+        
+        VStack {
+            Button(action: {
+                self.pauseTapped()
+            }) {
+                Text("Pause / Play")
+                    .font(.title)
+                    .frame(width: 200, height: 80)
+                    .background(Color.blue)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+            }
+            Slider(
+                value: $speed,
+                in: -20...20,
+                onEditingChanged: { editing in
+                    isEditing = editing
+                }
+            )
+            Text("\(words[displayIndex])")
+        }.onAppear {
+            // First, we're just iterating through with no control
+            let increment = 1
+            let decrement = -1
+            
+            // Run a background task with a delay when the view appears
+            var sleepTime = 1000000
+            DispatchQueue.global().async {
+                while true {
+                    if !isPaused {
+                        sleepTime = speed == 0 ? 0 : abs(Int(1000000 / speed))
+                        usleep(useconds_t(sleepTime))
+                    } else {
+                        while isPaused {
+                            usleep(useconds_t(1000000))
+                        }
+                    }
+                    sleepTime = speed == 0 ? 0 : abs(Int(1000000 / speed))
+                    usleep(useconds_t(sleepTime))
+                    if speed > 0.0 && self.displayIndex < words.count - 1 {
+                        self.displayIndex += increment
+                    } else if speed < 0  && self.displayIndex > 0 {
+                        self.displayIndex += decrement
+                    }
+                    //Thread.sleep(forTimeInterval: 1.0) // Sleep for 1 second
+                }
+            }
+        }
+    }
+    
+    private func pauseTapped() {
+        isPaused = isPaused ? false : true
+        // Perform action when enter key is tapped
+        //entered_entry = text // Store
+        //text = ""  // Reset
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Text-Brain-Interface/Text_Brain_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_Brain_InterfaceApp.swift
+//  Text-Brain-Interface
+//
+//  Created by Vincent Steffens on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Text_Brain_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
